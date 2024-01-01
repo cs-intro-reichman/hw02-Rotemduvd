b50a484cea124db561beccfe8e22243b017688a0
Rotemduvd <duvdevani.rotem@gmail.com>
2024-01-01 20:23:46 +0200

    ver2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,25 +1,22 @@
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
+
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 
-		int i = 0;
-		while (i < n) {
-			int j = 0;
-			while (j < n) {
-				System.out.print(" * ");
-				j++;
-			}if(i % 2 == 0){
-				System.out.println();
-				System.out.print(" ");
-			} else {
-				System.out.println();}
 		
-			i++;
+		for (int i = 1;i <= n; i++) { //prints number of rows until n
+			
+			for (int j = 1;j < n; j++){ // prints the row until j=n
+				if(i % 2 == 0){
+					System.out.print(" *");
+				}else {
+					System.out.print("* ");
+				} 
+		 
+			
+			} System.out.println();
+
+
 		}
-
-
 	}
 }
