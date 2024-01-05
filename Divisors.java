5d4aae6d4d92e77c43debfda0cd4160bad8bf1f5
Avielni222 <155298746+Avielni222@users.noreply.github.com>
2024-01-05 19:11:47 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -5,12 +5,16 @@ public class Divisors {
 	public static void main (String[] args) {
 		int x = Integer.parseInt(args[0]);
 		int d = x;
-
 		while (0 < d){
 		    if ((x % d) == 0){
 			    System.out.println(x / d);
 			}
 			d = d - 1;
 		}
+
+		/* you should have used a for loop and not while,
+		when we need to run a finite number of iterations like d, we use for 
+  		for (int d = x;d > 0; d--)
+  		*/
 	}
 }
